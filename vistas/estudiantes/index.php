40a1db8cd3742abcbdd1bd58065f96473551ad5a
kenser <koffy140221@gmail.com>
2023-05-21 23:04:45 -0600

    CAMBIO FINAL

--- vistas/estudiantes/index.php
+++ vistas/estudiantes/index.php
@@ -6,10 +6,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>FORMULARIO ESTUDIANTES</title>
+    <style>
+
+body {
+    background-image: url(./10.webp);
+    background-size:cover;
+    background-attachment: fixed;
+
+}
+
+/*diseño del tipo de texto h1*/
+h1 {
+    color: rgba(255, 255, 255, 0.948);
+    font-family: 'ALGERIAN';
+    text-shadow:2px 2px 1px rgb(1, 1, 1);
+  
+}
+/*diseño del tipo de texto h2*/
+h2 {
+    color: rgb(0, 0, 0);
+    text-align: center;
+    text-shadow: 2px 2px 2px whitesmoke;
+    border-color: azure;
+    border-radius: 4px;
+    font-family: 'calibri';
+    background: rgba(204, 202, 204, 0.705);
+    opacity: 0.7;
+    box-shadow: 1px 1px 10px gold;
+}
+/*diseño del contenedor*/
+.container {
+
+    justify-content: center;
+    width: 600px;
+}
+
+    </style>
 </head>
 <body>
     <div class="container">
-        <h1 class="text-center">FROMULARIO PARA INGRESAR ESTUDIANTES</h1>
+        <h1 class="text-center">FORMULARIO PARA INGRESAR ESTUDIANTES</h1>
         <div class="row justify-content-center">
             <form action="/practica07/controladores/estudiantes/guardar07.php" method="POST" class="col-lg-8 border bg-light p-3">
                 <div class="row mb-3">
